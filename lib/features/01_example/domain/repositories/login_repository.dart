c369cd1cd9f86f69f7c970f920c100148a5c3bb2
Thanutham Tuktian <thanutham.tn@gmail.com>
2025-07-24 01:51:20 +0700

    Remove example template

--- lib/features/01_example/domain/repositories/login_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:campus_life_hub/core/resources/data_state.dart';
-import '../entities/user_entity.dart';
-
-
-abstract class LoginRepository {
-  Future<DataState<UserEntity>> login(String email, String password);
-}
