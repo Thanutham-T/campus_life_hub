03153cfe364958c2dbfbef8f9d2440edf44e6574
6510110632 <nydukkung@gmail.com>
2025-07-31 19:07:16 +0700

    refactor: adjust widgets

--- lib/core/widgets/app_card.dart
+++ lib/core/widgets/app_card.dart
@@ -44,7 +44,7 @@ class AppCard extends StatelessWidget {
       borderRadius: BorderRadius.circular(AppSizes.radiusL),
       boxShadow: [
         BoxShadow(
-          color: Colors.black.withOpacity(0.08),
+          color: Colors.black.withValues(alpha: 0.08),
           blurRadius: 8,
           offset: const Offset(0, 2),
         ),
--- lib/core/widgets/widgets.dart
+++ lib/core/widgets/widgets.dart
@@ -2,7 +2,6 @@
 export 'app_button.dart';
 export 'app_card.dart';
 export 'app_dialog.dart';
-export 'app_feedback.dart';
 export 'app_text_field.dart';
 export 'empty_state.dart';
 export 'loading_view.dart';
