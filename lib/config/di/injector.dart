393f45ab093a4d0c84b05d618df8334aca5d144c
6510110632 <nydukkung@gmail.com>
2025-07-31 19:09:28 +0700

    feat: add dependency injection

---
+++ lib/config/di/injector.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// Feature DI imports
+import '../../features/user/di/injector.dart';
+
+final sl = GetIt.instance;
+
+/// Main dependency injection initialization
+Future<void> init() async {
+  //! Features
+  await _initFeatures();
+
+  //! Core
+  await _initCore();
+
+  //! External
+  await _initExternal();
+}
+
+/// Initialize all features
+Future<void> _initFeatures() async {
+  // User feature
+  await registerUserDI(sl);
+  
+  // Add other features here in the future
+  // await initDashboardFeature(sl);
+  // await initScheduleFeature(sl);
+}
+
+/// Initialize core dependencies
+Future<void> _initCore() async {
+  // Core utilities, constants, etc.
+  // Add any core dependencies here
+}
+
+/// Initialize external dependencies
+Future<void> _initExternal() async {
+  // Shared Preferences
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+
+  // Firebase Auth
+  sl.registerLazySingleton(() => FirebaseAuth.instance);
+}
---
+++ lib/features/user/di/injector.dart
@@ -0,0 +1,78 @@
+import 'package:get_it/get_it.dart';
+
+// User feature imports
+import '../data/datasources/local/auth_local_data_source.dart';
+import '../data/datasources/local/auth_local_data_source_impl.dart';
+import '../data/datasources/remote/auth_remote_data_source.dart';
+import '../data/datasources/remote/auth_remote_data_source_impl.dart';
+import '../data/repositories/auth_repository_impl.dart';
+import '../domain/repositories/auth_repository.dart';
+import '../domain/usecases/get_current_user.dart';
+import '../domain/usecases/login_user.dart';
+import '../domain/usecases/register_user.dart';
+import '../domain/usecases/logout_user.dart';
+import '../domain/usecases/update_profile.dart';
+import '../domain/usecases/change_password.dart';
+import '../domain/usecases/reset_password.dart';
+import '../presentation/bloc/auth_bloc.dart';
+
+/// User Feature Dependency Injection
+Future<void> registerUserDI(GetIt sl) async {
+  // Data sources (bottom layer)
+  sl.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(
+      firebaseAuth: sl.get(),
+    ),
+  );
+
+  sl.registerLazySingleton<AuthLocalDataSource>(
+    () => AuthLocalDataSourceImpl(
+      sharedPreferences: sl.get(),
+    ),
+  );
+
+  // Repository (middle layer)
+  sl.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(
+      remoteDataSource: sl.get<AuthRemoteDataSource>(),
+      localDataSource: sl.get<AuthLocalDataSource>(),
+    ),
+  );
+
+  // Use cases (top layer)
+  sl.registerLazySingleton<GetCurrentUser>(
+    () => GetCurrentUser(sl.get<AuthRepository>()),
+  );
+  sl.registerLazySingleton<LoginUser>(
+    () => LoginUser(sl.get<AuthRepository>()),
+  );
+  sl.registerLazySingleton<RegisterUser>(
+    () => RegisterUser(sl.get<AuthRepository>()),
+  );
+  sl.registerLazySingleton<LogoutUser>(
+    () => LogoutUser(sl.get<AuthRepository>()),
+  );
+  sl.registerLazySingleton<UpdateProfile>(
+    () => UpdateProfile(sl.get<AuthRepository>()),
+  );
+  sl.registerLazySingleton<ChangePassword>(
+    () => ChangePassword(sl.get<AuthRepository>()),
+  );
+  sl.registerLazySingleton<ResetPassword>(
+    () => ResetPassword(sl.get<AuthRepository>()),
+  );
+
+  // Bloc (presentation layer)
+  sl.registerFactory<AuthBloc>(
+    () => AuthBloc(
+      getCurrentUser: sl.get<GetCurrentUser>(),
+      loginUser: sl.get<LoginUser>(),
+      registerUser: sl.get<RegisterUser>(),
+      logoutUser: sl.get<LogoutUser>(),
+      updateProfile: sl.get<UpdateProfile>(),
+      changePassword: sl.get<ChangePassword>(),
+      resetPassword: sl.get<ResetPassword>(),
+    ),
+  );
+}
+
