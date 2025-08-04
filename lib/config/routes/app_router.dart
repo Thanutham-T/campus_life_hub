81ddd7630152f55efa551de8d39730bac8fb9f61
Thanutham Tuktian <thanutham.tn@gmail.com>
2025-08-04 11:03:30 +0700

    Remove unused import of main.dart in app_router.dart

--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -12,9 +12,6 @@ import '../../features/campus_event/presentation/pages/event_page.dart';
 import '../../features/study_group/presentation/pages/study_group_page.dart';
 import '../../features/campus_map/presentation/pages/campus_map_page.dart';
 import '../../features/announcement/presentation/pages/announcement_page.dart';
-import '../../main.dart';
-
-// Import dashboard_page แบบตรงๆ
 import '../../features/dashboard/presentation/pages/dashboard_page.dart';
 
 
