b587038a380c7f7fad7f3b87e5139969b56720d8
Pakkaphoom Jindadech <pakkaphoom.pooh123@hotmail.com>
2025-08-04 11:15:29 +0700

    Add flutter_slidable dependency and update get_it version; comment out unused schedule imports

--- lib/config/di/injector.dart
+++ lib/config/di/injector.dart
@@ -1,12 +1,12 @@
 import 'package:get_it/get_it.dart';
 
-import 'package:campus_life_hub/features/schedule/di/schedule_di.dart';
+// import 'package:campus_life_hub/features/schedule/di/schedule_di.dart';
 import 'package:campus_life_hub/features/course/di/course_di.dart';
 
 
 final GetIt sl = GetIt.instance;
 
 Future<void> init() async {
-  await registerScheduleDI();
+  // await registerScheduleDI();
   await registerCourseDI();
 }
