1a7b6a119c5bfcbd197441dc0f56b9076a506532
Thanutham Tuktian <thanutham.tn@gmail.com>
2025-08-11 21:10:28 +0700

    feat: update constant exports and clean up register page imports

--- lib/core/core_modules.dart
+++ lib/core/core_modules.dart
@@ -1,5 +1,5 @@
 // Core exports
-export 'constants/constant_modules.dart';
+export './constants/constant_modules.dart';
 export './errors/error_modules.dart';
 // export './extensions/extension_modules.dart';
 export './utils/util_modules.dart';
