528432e7b74f12da3d6626b68515dab2d96be715
Thanutham Tuktian <thanutham.tn@gmail.com>
2025-08-16 23:10:54 +0700

    refactor: comment out debug logging in route middleware for cleaner code

--- lib/config/routes/middleware.dart
+++ lib/config/routes/middleware.dart
@@ -10,8 +10,8 @@ class Middleware {
     // get the route path
     var path = state.uri.path;
 
-    // log the application path
-    AppLogger.debug(path.toString());
+    // // log the application path
+    // AppLogger.debug(path.toString());
 
     // check the route path and return boolean
     bool isPublicPath = path == Routes.register || path == Routes.login || path == Routes.onboarding || path == Routes.splash;
@@ -19,9 +19,9 @@ class Middleware {
     // get the user registered token
     String? token = await FirebaseAuth.instance.currentUser?.getIdToken();
 
-    // check the boolean conditions
-    AppLogger.debug('Route path is: ${isPublicPath.toString()}');
-    AppLogger.debug('User token: ${token.toString()}');
+    // // check the boolean conditions
+    // AppLogger.debug('Route path is: ${isPublicPath.toString()}');
+    // AppLogger.debug('User token: ${token.toString()}');
 
     // conditions to check the routes
     if(isPublicPath && (token != null || (token?.isNotEmpty ?? false))) {
